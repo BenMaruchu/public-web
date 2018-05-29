2e42bedb3164d9880fbb93c2a46ef8efa706a368
Benson Maruchu <benmaruchu@gmail.com>
2018-05-29 12:18:11 +0300

    Fix #1 : Read env variables from .env file
    - Change webpack environment plugin to use Dotenv
    - Remove dist files

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -3,6 +3,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const flexFix = require('postcss-flexbugs-fixes');
 const autoprefixer = require('autoprefixer');
 const webpack = require('webpack');
+const Dotenv = require('dotenv-webpack');
 
 const URL = 'https://dawasco-public.herokuapp.com/';
 // const URL = 'http://127.0.0.1:3000/';
@@ -102,10 +103,7 @@ const clientConfig = {
   },
   /* eslint indent:0 */
   plugins: [
-    new webpack.EnvironmentPlugin([
-      'NODE_ENV',
-      'PUBLICWEB_AUTH_TOKEN'
-    ]),
+    new Dotenv(),
     new ExtractTextPlugin({
       filename: 'styles.css',
     }),
