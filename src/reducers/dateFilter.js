2a7b90af9699e8cfc347c63794f9f5be0e20c36d
Navish <navish45@gmail.com>
2018-05-29 22:23:26 +0300

    Add unit test for date filter reducer
    - Changed some aliases to absolute paths

--- src/reducers/dateFilter.js
+++ src/reducers/dateFilter.js
@@ -1,11 +1,15 @@
-import { MAP_DATE_FILTER_CHANGE } from 'actions';
+import { MAP_DATE_FILTER_CHANGE } from '../actions/index';
 
 import moment from 'moment';
 
 const startDate = moment().subtract(1, 'months').startOf('date');
 const endDate = moment();
 
-const mapFilter = (state = { startDate, endDate }, action) => {
+export const initialState = {
+  startDate, endDate
+};
+
+const mapFilter = (state = initialState, action) => {
   switch (action.type) {
     case MAP_DATE_FILTER_CHANGE:
       return { ...state, startDate: action.startDate, endDate: action.endDate };
---
+++ src/reducers/dateFilter.test.js
@@ -0,0 +1,21 @@
+import dateFilter, { initialState } from './dateFilter';
+import MAP_DATE_FILTER_CHANGE from '../actions/index';
+import moment from 'moment';
+
+describe('Date Filter', () => {
+
+  const startDate = moment().subtract(1, 'months').startOf('date');
+  const endDate = moment();
+
+  it('should return the initial state', () => {
+    expect(dateFilter(undefined, {})).toEqual(initialState);
+  });
+
+  it('should handle MAP_DATE_FILTER_CHANGE action', () => {
+    expect(dateFilter({ startDate, endDate }, {
+      type: MAP_DATE_FILTER_CHANGE,
+      startDate: startDate,
+      endDate: endDate
+    })).toEqual({ startDate, endDate });
+  });
+});
