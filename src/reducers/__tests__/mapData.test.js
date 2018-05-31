727f055710c93d0961c9b4171f5915efab564c4a
Navish <navish45@gmail.com>
2018-05-31 23:12:04 +0300

    Add unit test for mapData reducer

---
+++ src/reducers/__tests__/mapData.test.js
@@ -0,0 +1,32 @@
+import { FETCH_MAP_DATA, FETCH_MAP_DATA_COMPLETE } from './../../actions/index';
+import mapData from './../mapData';
+
+describe('Map Data Test', () => {
+
+  it('should return initial state', () => {
+    const initialState = { loading: false, dataFound: true }
+    expect(mapData(undefined, {})).toEqual(initialState);
+  });
+
+  it('should handle FETCH_MAP_DATA action', () => {
+    const initialState = { loading: false, title: '' }
+    const expectOutput = { loading: true, title: '' }
+
+    expect(mapData(initialState, {
+      type: FETCH_MAP_DATA,
+      loading: true,
+      title: ''
+    })).toEqual(expectOutput);
+  });
+
+  it('should handle FETCH_MAP_DATA_COMPLETE action', () => {
+    const initialState = { loading: true, dataFound: false }
+    const expectOutput = { loading: false, dataFound: true }
+    expect(mapData(initialState, {
+      type: FETCH_MAP_DATA_COMPLETE,
+      loading: false,
+      dataFound: true
+    })).toEqual(expectOutput);
+  });
+
+})
