0f324c0e0f638b861842efb5d540260fa159387e
Navish <navish45@gmail.com>
2018-06-05 16:48:03 +0300

    Fix eslint errors

--- src/reducers/__tests__/serviceFilter.test.js
+++ src/reducers/__tests__/serviceFilter.test.js
@@ -9,70 +9,25 @@ describe('Jurisdiction Filter ', () => {
   });
 
   it('should handle RECEIVE_SERVICES action', () => {
-    const previousState = {
-      services: [
-        {
-          id: 1,
-          name: 'Test'
-        }
-      ]
-    };
-    const action = {
-      type: RECEIVE_SERVICES,
-      services: previousState.services
-    }
+    const previousState = { services: [{ id: 1, name: 'Test' }] };
+    const action = { type: RECEIVE_SERVICES, services: previousState.services };
+
     expect(serviceFilter(previousState, action)).toEqual(previousState);
   });
 
   it('should handle TOGGLE_SERVICE action', () => {
-    const previousState = {
-      services: [
-        {
-          id: 1,
-          name: 'Test'
-        }
-      ]
-    };
-    const expectOutput = {
-      services: [
-        {
-          id: 1,
-          name: 'Test',
-          selected: true
-        }
-      ]
-    };
-    const action = {
-      type: TOGGLE_SERVICE,
-      id: 1
-    };
-    expect(serviceFilter(previousState, action)).toEqual(expectOutput)
+    const previousState = { services: [{ id: 1, name: 'Test' }] };
+    const expectOutput = { services: [{ id: 1, name: 'Test', selected: true }] };
+    const action = { type: TOGGLE_SERVICE, id: 1 };
+
+    expect(serviceFilter(previousState, action)).toEqual(expectOutput);
   });
 
   it('should handle RESET_SERVICE action', () => {
-    const previousState = {
-      services: [
-        {
-          id: 1,
-          name: 'Test',
-          selected: true
-        }
-      ]
-    };
-    const expectOutput = {
-      services: [
-        {
-          id: 1,
-          name: 'Test',
-          selected: false
-        }
-      ]
-    };
-    const action = {
-      type: TOGGLE_SERVICE,
-      id: 1
-    };
-    expect(serviceFilter(previousState, action)).toEqual(expectOutput)
-  });
+    const previousState = { services: [{ id: 1, name: 'Test', selected: true }] };
+    const expectOutput = { services: [{ id: 1, name: 'Test', selected: false }] };
+    const action = { type: RESET_SERVICES, id: 1 };
 
+    expect(serviceFilter(previousState, action)).toEqual(expectOutput);
+  });
 });
